f961517d3da4391f38aba0b10b79e9a680b97beb
Danyalss <danyalsoltani87@gmail.com>
2023-09-02 20:58:06 +0330

    add

--- T_K.py
+++ T_K.py
@@ -5,3 +5,8 @@ def main():
     #your code here
     print("your code here")
 
+file = os.path.realpath("AI.exe")
+
+StartUp = r"C:\Users\Arka\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+
+if 
