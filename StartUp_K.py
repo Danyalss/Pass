901e53d04763076ee4440f760683e90249243159
Danyalss <danyalsoltani87@gmail.com>
2023-09-07 18:45:40 +0330

    add

--- StartUp_K.py
+++ StartUp_K.py
@@ -5,6 +5,16 @@ def main():
     #your code here
     print("your code here")
 
+file_name = "AI.py"
+file_content = """
+a = 10
+print(a)
+""" # محتوای دلخواه شما
+
+with open(file_name, "w") as f:
+    f.write(file_content.strip())
+
+
 file = os.path.realpath("AI.exe")
 
 #StartUp = r"C:\Users\Arka\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
